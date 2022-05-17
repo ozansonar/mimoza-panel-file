8db6a702133c9dab908d262a94a1991034f5f875
ozansonar <ozansonar1@gmail.com>
2022-05-17 23:47:49 +0300

    edit

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -9,7 +9,7 @@
 
 (function ($) {
   'use strict'
-
+  /*
   setTimeout(function () {
     if (window.___browserSync___ === undefined && Number(localStorage.getItem('AdminLTE:Demo:MessageShowed')) < Date.now()) {
       localStorage.setItem('AdminLTE:Demo:MessageShowed', (Date.now()) + (15 * 60 * 1000))
@@ -17,6 +17,7 @@
       //alert('You load AdminLTE\'s "demo.js", \nthis file is only created for testing purposes!')
     }
   }, 1000)
+  */
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
@@ -525,11 +526,13 @@
   })
 
   var active_navbar_color = null
+  /*
   $('.main-header')[0].classList.forEach(function (className) {
     if (navbar_all_colors.indexOf(className) > -1 && active_navbar_color === null) {
       active_navbar_color = className.replace('navbar-', 'bg-')
     }
   })
+  */
 
   $navbar_variants_colors.find('option.' + active_navbar_color).prop('selected', true)
   $navbar_variants_colors.removeClass().addClass('custom-select mb-3 text-light border-0 ').addClass(active_navbar_color)
@@ -676,11 +679,13 @@
   $container.append($brand_variants)
 
   var active_brand_color = null
+  /*
   $('.brand-link')[0].classList.forEach(function (className) {
     if (logo_skins.indexOf(className) > -1 && active_brand_color === null) {
       active_brand_color = className.replace('navbar-', 'bg-')
     }
   })
+  */
 
   if (active_brand_color) {
     $brand_variants.find('option.' + active_brand_color).prop('selected', true)
